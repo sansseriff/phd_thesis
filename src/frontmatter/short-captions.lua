1e6cafb16c1d45466309be65fa48e0d07e7af834
Andrew Mueller <amueller@caltech.edu>
2023-01-23 16:51:38 -0800

    overleaf update

--- src/frontmatter/short-captions.lua
+++ src/frontmatter/short-captions.lua
@@ -3,7 +3,7 @@ if FORMAT ~= "latex" then
   end
 
   function fix_path (path)
-    chapter = path:sub(8)
+    chapter = path:sub(3)
     return chapter
     -- return 'chapter_' .. chapter .. '/' .. path:sub(2,-5) .. '.pdf'
   end
