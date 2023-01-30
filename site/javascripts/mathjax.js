bf67e36fa7a455c1de1b813a6f10977f317c765b
Andrew Mueller <amueller@caltech.edu>
2023-01-30 15:20:08 -0800

    upgreek 2

--- site/javascripts/mathjax.js
+++ site/javascripts/mathjax.js
@@ -1,16 +1,26 @@
 window.MathJax = {
+    loader: { load: ['[tex]/upgreek'] },
     tex: {
         inlineMath: [["\\(", "\\)"]],
         displayMath: [["\\[", "\\]"]],
         processEscapes: true,
         processEnvironments: true,
         packages: { '[+]': ['upgreek'] },
+        autoload: {
+            upgreek: ['upalpha', 'upbeta', 'upchi', 'updelta', 'Updelta', 'upepsilon',
+                'upeta', 'upgamma', 'Upgamma', 'upiota', 'upkappa', 'uplambda',
+                'Uplambda', 'upmu', 'upnu', 'upomega', 'Upomega', 'upomicron',
+                'upphi', 'Upphi', 'uppi', 'Uppi', 'uppsi', 'Uppsi', 'uprho',
+                'upsigma', 'Upsigma', 'uptau', 'uptheta', 'Uptheta', 'upupsilon',
+                'Upupsilon', 'upvarepsilon', 'upvarphi', 'upvarpi', 'upvarrho',
+                'upvarsigma', 'upvartheta', 'upxi', 'Upxi', 'upzeta']
+        },
     },
     options: {
         ignoreHtmlClass: ".*|",
         processHtmlClass: "arithmatex"
     },
-    loader: { load: ['[tex]/upgreek'] },
+
 };
 
 document$.subscribe(() => {
