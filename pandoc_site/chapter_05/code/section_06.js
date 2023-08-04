2d6e7ca57024fc3543e818d014b58d3d2dfac9b2
Andrew Mueller <amueller@caltech.edu>
2023-08-04 13:06:14 -0700

    pretty formatting for citations

--- pandoc_site/chapter_05/code/section_06.js
+++ pandoc_site/chapter_05/code/section_06.js
@@ -53,6 +53,58 @@ function dmode_b(e) {
     switchMode('dark_img', 'light_img');
 }
 
+function formatText(text) {
+    // Replace special character codes with actual HTML tags
+    text = text.replace(/&lt;b&gt;/g, "<b>").replace(/&lt;\/b&gt;/g, "</b>");
+    text = text.replace(/&lt;i&gt;/g, "<i>").replace(/&lt;\/i&gt;/g, "</i>");
+    text = text.replace(/&lt;em&gt;/g, "").replace(/&lt;\/em&gt;/g, "");
+    text = text.replace(/&#160;/g, " ");
+    // Remove quotation marks and newlines
+    text = text.replace(/"/g, "").replace(/\n/g, "");
+    return text;
+}
+
+
+
+// Find all list elements with an id of the form "fn:1", "fn:2", etc.
+// and do some processing to correct garbled HTML in the footnotes
+document.addEventListener("DOMContentLoaded", function () {
+    const listElements = document.querySelectorAll("li[id^='fn:']");
+
+    // Loop through each list element
+    listElements.forEach((listElement) => {
+        // Find the <p> tag inside the list element
+        const pTag = listElement.querySelector("p");
+        if (pTag) {
+            // Get the text inside the <p> tag
+            let text = pTag.innerHTML;
+            text = formatText(text);
+            pTag.innerHTML = text;
+        }
+    });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const listElements = document.querySelectorAll("li[id^='fn:']");
+
+//     // Loop through each list element
+//     listElements.forEach((listElement) => {
+//         // Find the <p> tag inside the list element
+//         const pTag = listElement.querySelector("p");
+//         if (pTag) {
+//             // Get the text inside the <p> tag
+//             let text = pTag.textContent;
+//             // Format the text using the formatText() function
+//             text = formatText(text);
+//             // Set the text content of the <p> tag to an empty string
+//             pTag.textContent = "";
+//             // Prepend the formatted text to the existing innerHTML
+//             pTag.innerHTML = text + pTag.innerHTML;
+//         }
+//     });
+// });
+
+
 
 
 toggle_solutions()
---
+++ pandoc_site/javascripts/fix_footnotes.js
@@ -0,0 +1,30 @@
+function formatText(text) {
+    // Replace special character codes with actual HTML tags
+    text = text.replace(/&lt;b&gt;/g, "<b>").replace(/&lt;\/b&gt;/g, "</b>");
+    text = text.replace(/&lt;i&gt;/g, "<i>").replace(/&lt;\/i&gt;/g, "</i>");
+    text = text.replace(/&lt;em&gt;/g, "").replace(/&lt;\/em&gt;/g, "");
+    text = text.replace(/&#160;/g, " ");
+    // Remove quotation marks and newlines
+    text = text.replace(/"/g, "").replace(/\n/g, "");
+    return text;
+}
+
+
+
+// Find all list elements with an id of the form "fn:1", "fn:2", etc.
+// and do some processing to correct garbled HTML in the footnotes
+document.addEventListener("DOMContentLoaded", function () {
+    const listElements = document.querySelectorAll("li[id^='fn:']");
+
+    // Loop through each list element
+    listElements.forEach((listElement) => {
+        // Find the <p> tag inside the list element
+        const pTag = listElement.querySelector("p");
+        if (pTag) {
+            // Get the text inside the <p> tag
+            let text = pTag.innerHTML;
+            text = formatText(text);
+            pTag.innerHTML = text;
+        }
+    });
+});
