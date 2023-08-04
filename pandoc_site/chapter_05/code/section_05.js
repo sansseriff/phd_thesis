9ee6f7c55a4c3bc1ee7db560413c315113d6aa93
Andrew Mueller <amueller@caltech.edu>
2023-08-04 13:11:24 -0700

    navigtaion expand

--- pandoc_site/chapter_05/code/section_05.js
+++ pandoc_site/chapter_05/code/section_05.js
@@ -48,37 +48,7 @@ function dmode_b(e) {
     switchMode('dark_img', 'light_img');
 }
 
-function formatText(text) {
-    // Replace special character codes with actual HTML tags
-    text = text.replace(/&lt;b&gt;/g, "<b>").replace(/&lt;\/b&gt;/g, "</b>");
-    text = text.replace(/&lt;i&gt;/g, "<i>").replace(/&lt;\/i&gt;/g, "</i>");
-    text = text.replace(/&lt;em&gt;/g, "").replace(/&lt;\/em&gt;/g, "");
-    text = text.replace(/&#160;/g, " ");
-    // Remove quotation marks and newlines
-    text = text.replace(/"/g, "").replace(/\n/g, "");
-    return text;
-}
 
-
-
-// Find all list elements with an id of the form "fn:1", "fn:2", etc.
-const listElements = document.querySelectorAll("li[id^='fn:']");
-
-// Loop through each list element
-listElements.forEach((listElement) => {
-    // Find the <p> tag inside the list element
-    const pTag = listElement.querySelector("p");
-    console.log(pTag)
-    if (pTag) {
-        // Get the text inside the <p> tag
-        let text = pTag.textContent;
-        // Format the text using the formatText() function
-        text = formatText(text);
-        console.log(text)
-        // Replace the text inside the <p> tag with the formatted text
-        pTag.textContent = text;
-    }
-});
 
 toggle_solutions()
 init_img()
