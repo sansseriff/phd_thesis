f2d843754d37fb5b8ddc6e88e51f509ac7967df6
Andrew Mueller <amueller@caltech.edu>
2023-10-10 13:12:50 -0700

    testing cors

--- pandoc_site/javascripts/fix_footnotes.js
+++ pandoc_site/javascripts/fix_footnotes.js
@@ -11,18 +11,53 @@ function formatText(text) {
     return text;
 }
 
+var thing = null
 
+// // Find all list elements with an id of the form "fn:1", "fn:2", etc.
+// // and do some processing to correct garbled HTML in the footnotes
+// document.addEventListener("DOMContentLoaded", function () {
+//     const listElements = document.querySelectorAll("li[id^='fn:']");
+//     thing = listElements
+//     // console.log(listElements)
+//     // Loop through each list element
+//     listElements.forEach((listElement) => {
+//         // Find the <p> tag inside the list element
+//         const pTag = listElement.querySelector("p");
+//         const aTag = listElement.querySelector("a");
+//         if (pTag) {
+//             // Get the text inside the <p> tag
+//             let text = pTag.innerHTML;
+//             text = formatText(text);
+//             pTag.innerHTML = text;
+//         }
+//     });
+// });
 
 // Find all list elements with an id of the form "fn:1", "fn:2", etc.
 // and do some processing to correct garbled HTML in the footnotes
 document.addEventListener("DOMContentLoaded", function () {
     const listElements = document.querySelectorAll("li[id^='fn:']");
-
+    thing = listElements
+    // console.log(listElements)
     // Loop through each list element
     listElements.forEach((listElement) => {
         // Find the <p> tag inside the list element
         const pTag = listElement.querySelector("p");
-        if (pTag) {
+        const aTag = listElement.querySelector("a");
+        if (pTag && aTag) {
+            // Get the link inside the <a> tag
+            const url = aTag.href;
+            // Fetch the header content from the URL using the HEAD method
+            fetch(url, { method: 'HEAD' })
+                .then(response => {
+                    // Get the headers from the response
+                    const headers = response.headers;
+                    // Get the content of the "og:image" meta tag from the headers
+                    const ogImage = headers.get("og:image");
+                    // Do something with the og:image content
+                    console.log(ogImage);
+                })
+                .catch(error => console.error(error));
             // Get the text inside the <p> tag
             let text = pTag.innerHTML;
             text = formatText(text);
